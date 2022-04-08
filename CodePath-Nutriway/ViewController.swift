b36a0e13ae49e16410c6113bd4d66accc6b830ba
Leslie Segovia <segovialeslie1030@gmail.com>
2022-04-07 20:37:47 -0700

    Initial Commit / Minimal Login Design
    Created the project and added a log in design on the storyboard and navigation controller. Still need to connect and implement

---
+++ CodePath-Nutriway/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodePath-Nutriway
+//
+//  Created by Leslie Segovia on 4/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
