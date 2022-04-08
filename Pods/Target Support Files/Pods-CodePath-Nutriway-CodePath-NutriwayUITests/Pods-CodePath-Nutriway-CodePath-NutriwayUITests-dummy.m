fbbf5dacff24f5286afcc73e876957ff785a1593
reku-su <80661019+reku-su@users.noreply.github.com>
2022-04-08 05:05:43 -0700

    configure parse server

---
+++ Pods/Target Support Files/Pods-CodePath-Nutriway-CodePath-NutriwayUITests/Pods-CodePath-Nutriway-CodePath-NutriwayUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CodePath_Nutriway_CodePath_NutriwayUITests : NSObject
+@end
+@implementation PodsDummy_Pods_CodePath_Nutriway_CodePath_NutriwayUITests
+@end
