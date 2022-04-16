add1c83e6d081ee2c16682fd68ef8a3d0af197ba
Leslie Segovia <segovialeslie1030@gmail.com>
2022-04-16 00:51:09 -0700

    added new recipe screen

---
+++ CodePath-Nutriway/NewRecipeController.swift
@@ -0,0 +1,29 @@
+//
+//  NewRecipeController.swift
+//  CodePath-Nutriway
+//
+//  Created by Leslie Segovia on 4/15/22.
+//
+
+import UIKit
+
+class NewRecipeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
